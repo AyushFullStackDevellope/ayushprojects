0f36ae34c80dbe1b21ae9f3c6957403c7509c69a
AyushFullStackDevelloper <158497283+AyushFullStackDevellope@users.noreply.github.com>
2024-06-14 04:07:00 -0700

    Add files via upload

--- kbcarray.js
+++ kbcarray.js
@@ -9,53 +9,53 @@ function letsBegin() {
 // using array question 
 let questions = [
     {
-        question: "01. Who is the king of cricket?",
-        answers: ["A. Virat Kohli", "B. Rohit Sharma", "C. MS Dhoni", "D. Sachin Tendulkar"],
+        question: "01. What is the capital city of Mongolia?",
+        answers: ["A. Ulaanbaatar", "B. Astana", "C. Kathmandu", "D. Hanoi"],
         correct: 0
     },
     {
-        question: "02. Who is the CEO of Google?",
-        answers: ["A. Sundar Pichai", "B. Satya Nadella", "C. Narendra Modi", "D. None of the above"],
-        correct: 0
-    },
-    {
-        question: "03. Who is the founder of Enjoy Programming?",
-        answers: ["A. Elon Musk", "B. Bill Gates", "C. Sundar Pichai", "D. Yogesh Lolusare sir"],
-        correct: 3
-    },
-    {
-        question: "04. Who is the CEO of Tesla?",
-        answers: ["A. Elon Musk", "B. Jeff Bezos", "C. Mark Zuckerberg", "D. Steve Jobs"],
-        correct: 0
-    },
-    {
-        question: "05. Who is the founder of Microsoft?",
-        answers: ["A. Elon Musk", "B. Bill Gates", "C. Ayush Nakade", "D. None of the above"],
-        correct: 1
-    },
-    {
-        question: "06. Who is known as father of Pakistan?",
-        answers: ["A. Sachin Tendulkar", "B. Mahatma Gandhi", "C. Virat Kohli", "D. Narendra Modi"],
+        question: "02. Who wrote the epic fantasy novel 'The Lord of the Rings'?",
+        answers: ["A. J.K. Rowling", "B. George R.R. Martin", "C. J.R.R. Tolkien", "D. C.S. Lewis"],
         correct: 2
     },
     {
-        question: "07. Who is the Brand Ambassador of Audi?",
-        answers: ["A. Elon Musk", "B. MS Dhoni", "C. Narendra Modi", "D. King Kohli"],
-        correct: 3
-    },
-    {
-        question: "08. Who is the founder of SpaceX?",
-        answers: ["A. Virat Kohli", "B. Jeff Bezos", "C. Elon Musk", "D. None of these"],
-        correct: 2
-    },
-    {
-        question: "09. Who is the founder of Facebook?",
-        answers: ["A. Elon Musk", "B. Mark Zuckerberg", "C. Bill Gates", "D. All of these"],
+        question: "03. Which planet is known as the Red Planet?",
+        answers: ["A. Jupiter", "B. Mars", "C. Saturn", "D. Venus"],
         correct: 1
     },
     {
-        question: "10. Who is the Brand Ambassador of Cricket?",
-        answers: ["A. Jay Shah", "B. Kapil Dev", "C. Virat Kohli", "D. Gautam Gambhir"],
+        question: "04. What is the hardest natural substance on Earth?",
+        answers: ["A. Diamond", "B. Graphite", "C. Quartz", "D. Tungsten"],
+        correct: 0
+    },
+    {
+        question: "05. Which of these famous scientists developed the theory of general relativity?",
+        answers: ["A. Isaac Newton", "B. Albert Einstein", "C. Nikola Tesla", "D. Stephen Hawking"],
+        correct: 1
+    },
+    {
+        question: "06. In which year did the French Revolution begin?",
+        answers: ["A. 1789", "B. 1776", "C. 1804", "D. 1812"],
+        correct: 0
+    },
+    {
+        question: "07. Who painted the ceiling of the Sistine Chapel?",
+        answers: ["A. Leonardo da Vinci", "B. Michelangelo", "C. Raphael", "D. Donatello"],
+        correct: 1
+    },
+    {
+        question: "08. What is the world's longest river?",
+        answers: ["A. Amazon River", "B. Nile River", "C. Yangtze River", "D. Mississippi River"],
+        correct: 0
+    },
+    {
+        question: "09. Who invented the World Wide Web (WWW)?",
+        answers: ["A. Tim Berners-Lee", "B. Steve Jobs", "C. Bill Gates", "D. Mark Zuckerberg"],
+        correct: 0
+    },
+    {
+        question: "10. What is the chemical symbol for the element lead?",
+        answers: ["A. Ld", "B. L", "C. Pb", "D. Pd"],
         correct: 2
     }
 ];
